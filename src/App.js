ca5214b757bc451c4d82aa73d1a80c68b7ad6c95
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-09-29 03:15:36 +0600

    texting this

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hellow</h1>
+      <h1>Hellow This is code tracker website</h1>
+      <h1>Checking all is ok or not</h1>
     </div>
   );
 }
